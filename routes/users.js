615bb01297c0a258aba628ef70ba80645b1719e6
Hieu <hieule246@gmail.com>
2016-03-19 06:31:19 +0000

    changing microservice: return ip, language and software

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var externalip = require('external-ip');
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(req.ip);
+    var obj = {};
+    // get ip address
+    var ip = (req.headers && req.headers['x-forwarded-for'])
+        || req.ip 
+        || req._remoteAddress 
+        || (req.connection && req.connection.remoteAddress);
+    // get language 
+    var lang = req.headers["accept-language"].split(',')[0];
+    // get software
+    var software = req.headers["user-agent"].match('[(](.*?)[)]')[1];
+    obj.ipaddress = ip;
+    obj.language = lang;
+    obj.software = software;
+    res.send(obj);
 });
 
 module.exports = router;
